d5f075cf5d8a46798cc1fc40dc575e54c5a0fab3
Nonato17 <160282168+Nonato17@users.noreply.github.com>
2025-05-15 22:04:46 -0300

    Update script.js

--- apiprova/script.js
+++ apiprova/script.js
@@ -1,6 +1,17 @@
-fetch('https://dummyjson.com/products/category/smartphones')
-  .then(response => response.json())
-  .then(d => d.products.forEach(p => 
-    console.log(`Nome: ${p.title}, Marca: ${p.brand}`)
-  ))
-  .catch(e => console.error('Erro:', e));
+const API_URL = 'https://dummyjson.com/products/category/smartphones';
+
+fetch(API_URL)
+  .then(response => {
+    const responseData = response.json();
+    return responseData;
+  })
+  .then(data => {
+    const products = data.products;
+    products.forEach(product => {
+      const productInfo = `Nome: ${product.title}, Marca: ${product.brand}`;
+      console.log(productInfo);
+    });
+  })
+  .catch(error => {
+    console.error('Erro:', error);
+  });
